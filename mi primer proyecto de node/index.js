02e859293690f553eeafbad0ada8a8a088fe5a1f
Maikel Gonzalez <gmaikel438@gmail.com>
2023-10-27 20:20:31 -0500

    Ejecución y comentarios

---
+++ mi primer proyecto de node/index.js
@@ -0,0 +1,13 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto")
+
+/*
+    Esta es la parte intermedia de nuestro proyecto
+    En esta parte realizamos los chequeos necesarios
+    Por ultimo vamos a la ultima linea de saludo
+    Esto tambien esta comentado
+    Aqui tambien
+*/
+
+// Este va a ser el mensaje que se envia para despedir
+console.log("Adios")
